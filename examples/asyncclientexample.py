f948edff7570b8812061ed5f8317fc045ea296aa
Alek Ratzloff <alekratz@gmail.com>
2015-06-08 14:25:59 -0400

    Add asynchronous thin server/client examples

---
+++ examples/asyncclientexample.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from pythinclient.client import BasicThinClient
+
+if __name__ == "__main__":
+  client = BasicThinClient(65000)
+  client.connect()
+  while 1:
+    try:
+      line = input("> ")
+    except (KeyboardInterrupt, EOFError):
+      break
+    if line == "": continue
+    # process the line with the client
+    try:
+      # attempt to connect, send, receive, and close interactions with the server
+      client.send(line)
+      response = client.wait_receive()
+    except Exception as ex:
+      response = "Could not make a connection to the server\n"
+      response += "reason: %s" % ex
+    print(response)
+  client.close()
---
+++ examples/asyncserverexample.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from pythinclient.server import AsyncThinServer
+
+def server_help(msg, conn, addr):
+  conn.send(
+    """
+    Available commands:
+    help      : shows this help
+    echo      : send a message back to the client
+    log       : log a message to the server.log file
+    """.encode('ascii'))
+
+def server_echo(msg, conn, addr):
+  conn.send((msg + '\n').encode('ascii'))
+
+def server_log(msg, conn, addr):
+  # write the given message to the logfile
+  with open("server.log", "a+") as fp:
+    fp.write(msg + '\n')
+
+if __name__ == "__main__":
+  from sys import argv
+  daemon = True if "-d" in argv or "--daemon" in argv else False
+  server = AsyncThinServer(is_daemon=daemon)
+
+  # add hooks
+  server.add_hook('help', server_help)
+  server.add_hook('echo', server_echo)
+  server.add_hook('log', server_log)
+
+  # start it up
+  server.start()
